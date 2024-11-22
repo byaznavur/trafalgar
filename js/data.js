e432604832a9bad140e961623b5426bfebbabb94
azizdev28 <az1z.bakht1yarvich@gmail.com>
2024-11-22 18:28:53 +0500

    Next sections

---
+++ js/data.js
@@ -0,0 +1,37 @@
+const services = [
+  {
+    img: "../assets/image/search.png",
+    title: "Search doctor",
+    description:
+      "Choose your doctor from thousands of specialist, general, and trusted hospitals",
+  },
+  {
+    img: "../assets/image/img2.png",
+    title: "Online pharmacy",
+    description:
+      "Buy your medicines with our mobile application with a simple delivery system",
+  },
+  {
+    img: "../assets/image/img3.png",
+    title: "Consultation",
+    description:
+      "Free consultation with our trusted doctors and get the best recommendations",
+  },
+  {
+    img: "../assets/image/img4.png",
+    title: "Details info",
+    description:
+      "Free consultation with our trusted doctors and get the best recommendations",
+  },
+  {
+    img: "../assets/image/img5.png",
+    title: "Emergency care",
+    description:
+      "You can get 24/7 urgent care for yourself or your children and your lovely family",
+  },
+  {
+    img: "../assets/image/img6.png",
+    title: "Tracking",
+    description: "Track and save your medical history and health data",
+  },
+];
--- js/main.js
+++ js/main.js
@@ -1,6 +1,8 @@
 const menuBtn = document.querySelector(".menu-btn");
 const menuIcon = document.querySelector(".menu-btn i");
 const list = document.querySelector(".list");
+
+const servicesCards = document.querySelector(".services-cards");
 menuBtn.addEventListener("click", () => {
   if (menuIcon.classList.contains("bx-menu")) {
     menuIcon.classList.remove("bx-menu");
@@ -11,4 +13,18 @@ menuBtn.addEventListener("click", () => {
     menuIcon.classList.add("bx-menu");
     list.classList.remove("show");
   }
+});
+
+function getServecesCards(el) {
+  return ` <div class="card">
+            <img src=${el.img} alt="Search" />
+            <h3>${el.title}</h3>
+            <p>
+            ${el.description}
+            </p>
+          </div>`;
+}
+
+services.map((el) => {
+  servicesCards.innerHTML += getServecesCards(el);
 });
